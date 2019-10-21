ecd79d7baa890a572f5915f1b2d523e1d75772d5
381706AnastasiaPoletueva <nastenka1193@mail.ru>
2019-10-21 15:47:11 +0300

    remain

--- modules/task_1/poletueva_a_counting_the_number_of_words/poletueva_a_counting_the_number_of_words_mpi.cpp
+++ modules/task_1/poletueva_a_counting_the_number_of_words/poletueva_a_counting_the_number_of_words_mpi.cpp
@@ -52,12 +52,10 @@ int getCountWordsParallel(std::string str) {
 
   std::vector<char> vec;
   int delta;
-  int remain;
 
   if (rank == 0) {
     vec = std::vector<char>(str.begin(), str.end());
     delta = vec.size() / size;
-    remain = size - delta * (size - 1);
     for (int proc = 1; proc < size - 1; proc++) {
       MPI_Send(&delta, 1, MPI_INT, proc, 1, MPI_COMM_WORLD);
       MPI_Send(&vec[0] + proc * delta, delta, MPI_CHAR, proc, 0, MPI_COMM_WORLD);
